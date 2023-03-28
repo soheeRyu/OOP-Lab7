b9e9f4be9ce79648855c9b2be755c222249a8c28
soheeRyu <sohee.ryu@edu.sait.ca>
2023-03-28 10:14:02 -0600

    initial commit

---
+++ MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathLab
+{
+    public class MathOperations
+    {
+        public double Add(double a, double b)
+        {
+            return a+b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a-b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a*b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a/b;
+        }
+    }
+}
